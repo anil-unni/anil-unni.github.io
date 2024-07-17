ecce1046c9a59f602c164a57a66da87f52c24687
Anil Unni <anilunni@outlook.com>
2024-07-17 22:42:02 +0530

    updated

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -1,93 +1,91 @@
-import Image from "next/image";
-import styles from "./styles.module.css";
+"use client";
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 export default function Home() {
+
+  const [currentTagline, setCurrentTagline] = useState(0);
+
+  const taglines = [
+    "Innovative Web Developer",
+    "Passionate Tech Enthusiast",
+    "Creative Problem Solver",
+    "Design Thinker",
+    "Tech-Savvy Innovator",
+    "Full-Stack Developer",
+    "User-Centric Designer",
+    "Agile Developer",
+  ];
+
+  const handleScroll = (event:any) => {
+    if (event.deltaY > 0) {
+      setCurrentTagline((prev) => (prev + 1) % taglines.length);
+    } else {
+      setCurrentTagline((prev) => (prev - 1 + taglines.length) % taglines.length);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleScroll);
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Hello
-          <code className="font-mono font-bold"></code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <h1 className={styles.gradientText}>
-          I'm Anil
-        </h1>
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black p-4 overflow-hidden">
+      <motion.h2
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="text-2xl md:text-3xl text-white mb-4 text-center"
+        >Hi, I&apos;m</motion.h2>
+      <motion.h1
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="text-5xl md:text-7xl font-bold text-white mb-4 text-center"
+      >
+        Anil Unni
+      </motion.h1>
+      <motion.p
+        key={currentTagline}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        exit={{ opacity: 0, y: -50 }} // Dissolve effect on exit
+        className="text-2xl md:text-3xl text-white mb-8 text-center"
+      >
+        {taglines[currentTagline]}
+      </motion.p>
+      <div className="mt-8 space-x-4 flex">
         <a
-          href="https://github.com/anil-unni"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href='mailto:anilunni@outlook.com'
           target="_blank"
           rel="noopener noreferrer"
+          className="text-white hover:text-gray-200 text-3xl"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            GitHub{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Check out my projects and code contributions on GitHub.
-          </p>
+          <MdEmail />
+        </a>
+        <a
+          href="https://github.com/anil-unni"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:text-gray-200 text-3xl"
+        >
+          <FaGithub />
         </a>
         <a
           href="https://www.linkedin.com/in/anilunni"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
+          className="text-white hover:text-gray-200 text-3xl"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            LinkedIn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Connect with me on LinkedIn to expand our professional network.
-          </p>
-        </a>
-
-        <a
-          href="https://twitter.com/anilunni123"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Twitter{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Follow me on Twitter for updates and insights.
-          </p>
-        </a>
-
-        <a
-          href="https://www.instagram.com/anilunni123"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Instagram{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Check out my latest photos and stories on Instagram.
-          </p>
+          <FaLinkedin />
         </a>
       </div>
-    </main>
+    </div>
   );
 }
