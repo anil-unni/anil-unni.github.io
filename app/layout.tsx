7650f215e42829f82fe53ddcba0fe2c21af1531f
Anil Unni <anilunni@outlook.com>
2024-09-11 01:17:41 +0530

    zoom disable

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, width=device-width, height=device-height, target-densitydpi=device-dpi"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no"/>
           <meta name="robots" content="index, follow" />
           <meta name="author" content="Anil Unni" />
           <link rel="canonical" href="https://anilunni.me" />
