7b1c279c3ad2fbffc551adbd3862e67b94842a58
Anil Unni <anilunni@outlook.com>
2025-02-19 23:32:03 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.glb$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          outputPath: "static/models/",
+          publicPath: "/_next/static/models/",
+        },
+      },
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
